95b276e9097bc70ab2dabf393df7290b476b25f5
Dana <118026410+danajeon@users.noreply.github.com>
2024-05-10 16:33:31 -0400

    created button component (#6)
    * created button component
    
    * created button *revised
    
    * created button, fixed alignment

---
+++ app/components/button.tsx
@@ -0,0 +1,11 @@
+interface ButtonProps {
+    classNames: string;
+    text: string;
+    onClick: () => void;
+}
+
+const Button = ({classNames, text, onClick}: ButtonProps) => {
+    return <button className = {classNames} onClick = {() => onClick()}>
+                {text}
+           </button>
+};
