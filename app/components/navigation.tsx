725d3b948b3d908a2b5c8b7d40d29c1eb573afb7
Dana <118026410+danajeon@users.noreply.github.com>
2024-05-16 21:09:04 -0400

    create navigation component (#13)
    * created button component
    
    * created button *revised
    
    * created button, fixed alignment
    
    * added navigation component
    
    * created navigation component

---
+++ app/components/navigation.tsx
@@ -0,0 +1,10 @@
+const Navigation = () => {
+    return ( <nav>
+                <a href="/">Home</a>
+                <a href="/Menu">Menu</a>
+                <a href="/Reservation">Reservation</a>
+                <a href="/Pages">Pages</a>
+                <a href="/Contact">Contact</a>
+            </nav>
+           )
+}
