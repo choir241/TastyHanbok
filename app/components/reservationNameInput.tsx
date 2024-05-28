881841032126f6c9040cab79d6e3bd53e07ff021
Dana Jeon <danajeon@Danas-MacBook-Air.local>
2024-05-28 18:59:31 -0400

    created reservation name input component

---
+++ app/components/reservationNameInput.tsx
@@ -0,0 +1,15 @@
+import { GoPerson } from "react-icons/go";
+
+interface ReservationNameInputProps {
+    onChange: (e: string) => void;
+}
+
+export default function ReservationNameInput ({onChange}: ReservationNameInputProps){
+    return <fieldset>
+                <input 
+                    type="text" 
+                    onChange = {(e) => onChange(e.target.value)}
+                />
+                <GoPerson />
+           </fieldset>
+}
